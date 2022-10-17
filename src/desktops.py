36d50c289bf3321bb6434623101e57723caf018d
axtloss <axtlos@tar.black>
2022-10-17 10:25:26 +0200

    remove desktops
    Signed-off-by: axtloss <axtlos@tar.black>

--- src/desktops.py
+++ src/desktops.py
@@ -2,14 +2,7 @@ desktops = [
     "Onyx",
     "GNOME",
     "Plasma",
-    "Budgie",
     "XFCE",
-    "MATE",
-    "Cinnamon",
-    "LXQt",
     "Sway",
     "i3-gaps",
-    "herbstluftwm",
-    "AwesomeWM",
-    "bspwm"
 ]
