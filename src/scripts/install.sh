28bf9f79ce505f7882174c99093516bc45db4924
axtloss <axtlos@tar.black>
2022-10-18 14:53:08 +0200

    fix mirrorlist
    Signed-off-by: axtloss <axtlos@tar.black>

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 echo "Running reflector to sort for fastest mirrors"
-pkexec reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorl
+pkexec reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 pkexec jade config ~/.config/jade.json
